17082b999ac9e1fdb47bc03785fcd500aa634738
Benjamin Ellis <benjaminjellis@protonmail.com>
2025-08-27 21:20:51 +0100

    feat: set lualine jj background to orange

--- lua/lualine/themes/ns.lua
+++ lua/lualine/themes/ns.lua
@@ -10,37 +10,37 @@ end
 return {
   normal = {
     a = { bg = palette.statusline1, fg = palette.bg0, gui = "bold" },
-    b = { bg = palette.bg0, fg = palette.fg },
+    b = { bg = palette.orange, fg = palette.fg },
     c = { bg = palette.bg0, fg = palette.fg },
   },
   insert = {
     a = { bg = palette.statusline2, fg = palette.bg0, gui = "bold" },
-    b = { bg = palette.bg0, fg = palette.fg },
+    b = { bg = palette.orange, fg = palette.fg },
     c = { bg = palette.bg0, fg = palette.fg },
   },
   visual = {
     a = { bg = palette.statusline3, fg = palette.bg0, gui = "bold" },
-    b = { bg = palette.bg0, fg = palette.fg },
+    b = { bg = palette.orange, fg = palette.fg },
     c = { bg = palette.bg0, fg = palette.fg },
   },
   replace = {
     a = { bg = palette.orange, fg = palette.bg0, gui = "bold" },
-    b = { bg = palette.bg0, fg = palette.fg },
+    b = { bg = palette.orange, fg = palette.fg },
     c = { bg = palette.bg0, fg = palette.fg },
   },
   command = {
     a = { bg = palette.aqua, fg = palette.bg0, gui = "bold" },
-    b = { bg = palette.bg0, fg = palette.fg },
+    b = { bg = palette.orange, fg = palette.fg },
     c = { bg = palette.bg0, fg = palette.fg },
   },
   terminal = {
     a = { bg = palette.purple, fg = palette.bg0, gui = "bold" },
-    b = { bg = palette.bg0, fg = palette.fg },
+    b = { bg = palette.orange, fg = palette.fg },
     c = { bg = palette.bg0, fg = palette.fg },
   },
   inactive = {
     a = { bg = palette.bg1, fg = palette.grey1 },
-    b = { bg = palette.bg1, fg = palette.grey1 },
+    b = { bg = palette.orange, fg = palette.fg },
     c = { bg = palette.bg1, fg = palette.grey1 },
   },
 }
